b08b80d594a89531e7dc858de1ad571109763107
Taranjeet Singh <taranjeet@flick2know.com>
2022-11-16 13:03:36 +0530

    [Retry] Update retry policy.
    Signed-off-by: Taranjeet Singh <taranjeet@flick2know.com>

--- dist/src/share.js
+++ dist/src/share.js
@@ -32,33 +32,57 @@ exports.slackShare = void 0;
 const web_api_1 = require("@slack/web-api");
 const fs = __importStar(require("fs"));
 const slackShare = (SLACK_TOKEN, SHARE_FILE, SLACK_CHANNEL, FILE_PATH, SLACK_MESSAGE) => __awaiter(void 0, void 0, void 0, function* () {
-    const web = new web_api_1.WebClient('', {
+    const web = new web_api_1.WebClient("", {
         headers: { Authorization: `Bearer ${SLACK_TOKEN}` },
+        retryConfig: { retries: 1 },
+        rejectRateLimitedCalls: true,
     });
     var slackMessage = SLACK_MESSAGE;
     if (SHARE_FILE) {
         (() => __awaiter(void 0, void 0, void 0, function* () {
-            // Post a message to the channel, and await the result.
-            // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-            const result = yield web.files.upload({
-                channels: SLACK_CHANNEL,
-                initial_comment: slackMessage,
-                file: fs.createReadStream(FILE_PATH),
-            });
-            const file = result.file;
-            console.log(`Successfully sent  file: ${file.name} to ${SLACK_CHANNEL} channel with timestamp: ${file.timestamp}`);
+            try {
+                // Post a message to the channel, and await the result.
+                // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+                const result = yield web.files.upload({
+                    channels: SLACK_CHANNEL,
+                    initial_comment: slackMessage,
+                    file: fs.createReadStream(FILE_PATH),
+                });
+                const file = result.file;
+                console.log(`Successfully sent  file: ${file.name} to ${SLACK_CHANNEL} channel with timestamp: ${file.timestamp}`);
+            }
+            catch (e) {
+                if (e.code === web_api_1.ErrorCode.PlatformError) {
+                    console.log(e.data);
+                }
+                else {
+                    // Some other error, oh no!
+                    console.log("Well, that was unexpected.");
+                }
+            }
         }))();
     }
     else {
         (() => __awaiter(void 0, void 0, void 0, function* () {
-            // Post a message to the channel, and await the result.
-            // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-            const result = yield web.chat.postMessage({
-                text: slackMessage,
-                channel: SLACK_CHANNEL,
-            });
-            // The result contains an identifier for the message, `ts`.
-            console.log(`Successfully sent message to ${SLACK_CHANNEL} channel with timestamp: ${result.ts}`);
+            try {
+                // Post a message to the channel, and await the result.
+                // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+                const result = yield web.chat.postMessage({
+                    text: slackMessage,
+                    channel: SLACK_CHANNEL,
+                });
+                // The result contains an identifier for the message, `ts`.
+                console.log(`Successfully sent message to ${SLACK_CHANNEL} channel with timestamp: ${result.ts}`);
+            }
+            catch (e) {
+                if (e.code === web_api_1.ErrorCode.PlatformError) {
+                    console.log(e.data);
+                }
+                else {
+                    // Some other error, oh no!
+                    console.log("Well, that was unexpected.");
+                }
+            }
         }))();
     }
 });
--- src/share.ts
+++ src/share.ts
@@ -1,4 +1,4 @@
-import { WebClient } from "@slack/web-api";
+import { ErrorCode, WebClient } from "@slack/web-api";
 import * as fs from "fs";
 
 export const slackShare = async (
@@ -8,40 +8,60 @@ export const slackShare = async (
   FILE_PATH: string,
   SLACK_MESSAGE: string
 ) => {
-  const web = new WebClient('', {
+  const web = new WebClient("", {
     headers: { Authorization: `Bearer ${SLACK_TOKEN}` },
+    retryConfig: { retries: 1 },
+    rejectRateLimitedCalls: true,
   });
 
   var slackMessage = SLACK_MESSAGE;
 
   if (SHARE_FILE) {
     (async () => {
-      // Post a message to the channel, and await the result.
-      // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-      const result = await web.files.upload({
-        channels: SLACK_CHANNEL,
-        initial_comment: slackMessage,
-        file: fs.createReadStream(FILE_PATH),
-      });
+      try {
+        // Post a message to the channel, and await the result.
+        // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+        const result = await web.files.upload({
+          channels: SLACK_CHANNEL,
+          initial_comment: slackMessage,
+          file: fs.createReadStream(FILE_PATH),
+        });
 
-      const file = result.file as any;
-      console.log(
-        `Successfully sent  file: ${file.name} to ${SLACK_CHANNEL} channel with timestamp: ${file.timestamp}`
-      );
+        const file = result.file as any;
+        console.log(
+          `Successfully sent  file: ${file.name} to ${SLACK_CHANNEL} channel with timestamp: ${file.timestamp}`
+        );
+      } catch (e: any) {
+        if (e.code === ErrorCode.PlatformError) {
+          console.log(e.data);
+        } else {
+          // Some other error, oh no!
+          console.log("Well, that was unexpected.");
+        }
+      }
     })();
   } else {
     (async () => {
-      // Post a message to the channel, and await the result.
-      // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-      const result = await web.chat.postMessage({
-        text: slackMessage,
-        channel: SLACK_CHANNEL,
-      });
+      try {
+        // Post a message to the channel, and await the result.
+        // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+        const result = await web.chat.postMessage({
+          text: slackMessage,
+          channel: SLACK_CHANNEL,
+        });
 
-      // The result contains an identifier for the message, `ts`.
-      console.log(
-        `Successfully sent message to ${SLACK_CHANNEL} channel with timestamp: ${result.ts}`
-      );
+        // The result contains an identifier for the message, `ts`.
+        console.log(
+          `Successfully sent message to ${SLACK_CHANNEL} channel with timestamp: ${result.ts}`
+        );
+      } catch (e: any) {
+        if (e.code === ErrorCode.PlatformError) {
+          console.log(e.data);
+        } else {
+          // Some other error, oh no!
+          console.log("Well, that was unexpected.");
+        }
+      }
     })();
   }
 };
